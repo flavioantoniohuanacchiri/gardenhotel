c76f7478e9681f85bfdf284dc934a27c769e1c45
Andreu <andreu@Andreus-MacBook-Pro.local>
2018-01-06 17:41:54 -0500

    seedder class call

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call(PerfilModuloSeeder::class);
         $this->call(WebBannerSeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(SectionSeeder::class);
     }
 }
