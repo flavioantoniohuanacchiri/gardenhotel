7d4c8e3d94d0c6b50749a2d16dc291db32364a34
Andreu <andreu@Andreus-MacBook-Pro.local>
2018-01-06 17:46:06 -0500

    bug seed

--- database/seeds/SectionSeeder.php
+++ database/seeds/SectionSeeder.php
@@ -11,7 +11,7 @@ class SectionSeeder extends Seeder
      */
     public function run()
     {
-      DB::table('section')->insert(['section' =>1, 'ubicacion' => '<p>Avenida Ricardo Rivera Navarrete 450, San Isidro</p>', 'ubicacion' => '<p>Avenida Ricardo Rivera Navarrete 450, San Isidro(en)</p>', 'telefono'=> '<p>(511) 4421771 / 99574368566</p>', 'email'=>'']);
-      DB::table('section')->insert(['section' =>2, 'ubicacion' => '<p>Avenida Ricardo Rivera Navarrete 450, San Isidro</p>', 'ubicacion' => '<p>Avenida Ricardo Rivera Navarrete 450, San Isidro(en)</p>', 'telefono'=> '<p><strong>(511) 4421771 / 995743685</strong></p>', 'email'=>'<p><a href="mailto:reservas@gardenhotel.com.pe">reservas@gardenhotel.com.pe</a></p>']);
+      DB::table('sections')->insert(['section' =>1, 'ubicacion' => '<p>Avenida Ricardo Rivera Navarrete 450, San Isidro</p>', 'ubicacion' => '<p>Avenida Ricardo Rivera Navarrete 450, San Isidro(en)</p>', 'telefono'=> '<p>(511) 4421771 / 99574368566</p>', 'email'=>'']);
+      DB::table('sections')->insert(['section' =>2, 'ubicacion' => '<p>Avenida Ricardo Rivera Navarrete 450, San Isidro</p>', 'ubicacion' => '<p>Avenida Ricardo Rivera Navarrete 450, San Isidro(en)</p>', 'telefono'=> '<p><strong>(511) 4421771 / 995743685</strong></p>', 'email'=>'<p><a href="mailto:reservas@gardenhotel.com.pe">reservas@gardenhotel.com.pe</a></p>']);
     }
 }
